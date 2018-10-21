96a06382894b113b737d9300ec63d0ead76fe34b
Nikolai Grin <Nikolai_Grin@Mail.Ru>
2018-10-21 18:53:08 +0700

    Creating the class, constructor and setters

---
+++ valera.rb
@@ -0,0 +1,45 @@
+class Valera
+	attr_reader :health, :mana, :joy, :fatigue, :money
+	def initialize
+		@health, @mana, @joy, @fatigue, @money = 100, 0, 0, 0, 0
+	end
+	def health=(health)
+		if (health > 100)
+			@health = 100
+		elsif (health < 0)
+			@health = 0
+		else 
+			@health = health
+		end
+	end
+	def mana=(mana)
+		if (mana > 100)
+			@mana = 100
+		elsif (mana < 0)
+			@mana = 0
+		else
+			@mana = mana
+		end
+	end
+	def joy=(joy)
+		if (joy > 10)
+			@joy = 10
+		elsif (joy < -10)
+			@joy = -10
+		else
+			@joy = joy
+		end
+	end
+	def fatigue=(fatigue)
+		if (fatigue > 100)
+			@fatigue = 100
+		elsif (fagilue < 0)
+			@fatigue = 0
+		else
+			@fatigue = fatigue
+		end
+	end
+	def money=(money)
+		@money = money
+	end
+end
