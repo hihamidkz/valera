34594246d74ad52071e257dc830b17b921b1d4ab
hamid <hihamidkz@inbox.ru>
2018-10-22 21:22:03 +0700

    Created module for the commands method execute

--- command.rb
+++ command.rb
@@ -7,3 +7,26 @@ class Command
 	def execute
 	end
 end
+
+module CommandModule
+	def execute
+		conditions = @values['conditions']
+		
+		res = true
+		if conditions != nil then
+			conditions.each do |condition|
+				cond = Condition.new(condition['value1'], condition['oper'], condition['value2'])
+				
+				res &= cond.true?
+			end
+		end
+		
+		if (res) then
+			@valera.health += @values['health']
+			@valera.mana += @values['mana']
+			@valera.joy += @values['joy']
+			@valera.fatigue += @values['fatigue']
+			@valera.money += @values['money']
+		end
+	end
+end
