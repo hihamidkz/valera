6c837d48c34bebe8231a91f55054bae87eaaa8d5
Nikolai Grin <Nikolai_Grin@Mail.Ru>
2018-10-29 13:51:48 +0700

    Creating a method for reading Valera's actions

--- controller.rb
+++ controller.rb
@@ -6,4 +6,18 @@ class Controller
 		@commands = array
 		@view = view
 	end
+	def input
+		flag = 1
+		puts "Enter a command number ranging from 0 to #{@commands.length - 1}"
+		puts "Or enter -1 to exit"
+		while flag == 1 do
+			number = gets.chomp.to_i
+			if (number >= @commands.length and number >=-1)
+				puts "You have entered incorrect data. Repeat input"
+			else
+				flag = 0
+			end
+		end
+		return number
+	end
 end
